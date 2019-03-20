4c8ecc9acf1f324022c24661fdd4209635b8b7e1
siwat jomewattana <s1104300051612@gmail.com>
2019-03-20 19:30:51 +0700

    ฟังชั่นกรองข้อมูลจากรหัส id ของ Provider

--- src/providers/customer/customer.ts
+++ src/providers/customer/customer.ts
@@ -28,4 +28,14 @@ export class CustomerProvider {
     return Promise.resolve(this.data);  //ฟังชั่นการส่งค่าข้อมูลแบบ Promise
   }
 
+  getByID(id){ //ฟังชั่นกรองข้อมูลจากรหัส id
+    for(var i=0;i<(this.data).length;.i++){ //รับค่า id และ loop เช็คค่าที่เข้ามาว่ามากี่ข้อมูล
+
+      if(this.data[i].code==id){
+        return Promise.resolve(this.data[i]); 
+      }//เปรียบเทียบว่า code ที่ส่งเข้ามานั้นเท่ากันกับ id ของข้อมูลหรือไม่ถ้าเท่ากันถึงจะส่งข้อมูลของ id กลับไป
+
+    }
+  }
+
 }
