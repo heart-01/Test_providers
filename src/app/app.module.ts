ce53172f1d129c36ee1937f3d2371563436d1cee
siwat jomewattana <s1104300051612@gmail.com>
2019-03-20 17:20:08 +0700

    สร้าง link page กับ import provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,35 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {HttpClientModule} from '@angular/common/http'; //1.provider
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ContactPage } from '../pages/contact/contact';
+import { CustomerProvider } from '../providers/customer/customer'; //1.link page
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ContactPage //2.link page
   ],
   imports: [
+    HttpClientModule, //2.provider
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ContactPage //3.link page
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CustomerProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact'; // ถ้าจะให้ลิงก์ไปหน้าอื่นต้อง import หน้านั้นเข้ามาด้วย
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoContact(){
+    this.navCtrl.push(ContactPage);
   }
 
 }
